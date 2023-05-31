56ea138e76abec7faacdad30ca2105ce076a5470
Choijeongan <choijeongan@gmail.com>
2023-05-31 16:17:29 +0900

    2023.05.31 계좌잔액등록 완성

--- apps/views/base/custViews.py
+++ apps/views/base/custViews.py
@@ -19,48 +19,49 @@ def custViews_search(request):
     custYn = request.POST.get('custYn')
 
 
-    # if custCode is not None and custCode != '' or custType is not None and custType != '' or custYn is not None and custYn != '':
-    #     with connection.cursor() as cursor:
-    #         cursor.execute(
-    #             " SELECT IFNULL(CUST_NME, ''), IFNULL(CUST_NBR, ''), IFNULL(CUST_CEO_NME, ''), IFNULL(CUST_ID_NBR, '') "
-    #             "   , IFNULL(CUST_BSN_CON, ''), IFNULL(CUST_BSN_TYP, ''), IFNULL(CUST_POST_NBR, '') "
-    #             "   , IFNULL(CUST_ADDR, ''), IFNULL(CUST_TEL_NBR, ''), IFNULL(CUST_FAX_NBR, '') "
-    #             "   , IFNULL(CUST_EMP_NME, ''), IFNULL(CUST_EMP_PHN, ''), IFNULL(CUST_EMAIL, '') "
-    #             "   , IFNULL(CUST_HOMEP, ''), IFNULL(CUST_GBN, ''), IFNULL(D.RESNAM, ''), IFNULL(CUST_BUNR, '')"
-    #             "   , IFNULL(CUST_END_CHK, 'Y') "
-    #             "   FROM MIS1TB003 A "
-    #             "   LEFT OUTER JOIN OSREFCP D "
-    #             "   ON A.CUST_GBN = D.RESKEY "
-    #             "   AND D.RECODE = 'CGB' "
-    #             "   WHERE CUST_NBR LIKE '%" + custCode + "%' "
-    #             "   OR CUST_NME LIKE '%" + custCode + "%' "
-    #             "   AND CUST_GBN LIKE '%" + custType + "%' "
-    #             "   AND CUST_END_CHK LIKE '%" + custYn + "%' "
-    #         )
-    #         custresult = cursor.fetchall()
-    #
-    #     # 업체 구분 - 콤보박스
-    #     with connection.cursor() as cursor:
-    #         cursor.execute(" SELECT RESKEY, RESNAM FROM OSREFCP WHERE RECODE = 'CGB' ")
-    #         inputCustType = cursor.fetchall()
-    #
-    #     # 사용여부 - 콤보박스
-    #     with connection.cursor() as cursor:
-    #         cursor.execute(" SELECT RESKEY, RESNAM FROM OSREFCP WHERE RECODE = 'UST' ")
-    #         cboCustYn = cursor.fetchall()
-    #
-    #     # 업체 분류 - 콤보박스
-    #     with connection.cursor() as cursor:
-    #         cursor.execute(" SELECT RESKEY, RESNAM FROM OSREFCP WHERE RECODE = 'CGB' ")
-    #         cboCustType = cursor.fetchall()
-    #
-    #     # 업체군 분류 - 콤보박스
-    #     with connection.cursor() as cursor:
-    #         cursor.execute(" SELECT RESKEY, RESNAM FROM OSREFCP WHERE RECODE = 'CGB' ")
-    #         cboCustType2 = cursor.fetchall()
-    #
-    #     return JsonResponse({"inputCustType": inputCustType, "cboCustYn": cboCustYn, "cboCustType": cboCustType, "cboCustType2": cboCustType2
-    #                             , "custList": custresult})
+    if custCode is not None and custCode != '' or custType is not None and custType != '' or custYn is not None and custYn != '':
+        with connection.cursor() as cursor:
+            cursor.execute(
+                " SELECT IFNULL(CUST_NME, ''), IFNULL(CUST_NBR, ''), IFNULL(CUST_CEO_NME, ''), IFNULL(CUST_ID_NBR, '') "
+                "   , IFNULL(CUST_BSN_CON, ''), IFNULL(CUST_BSN_TYP, ''), IFNULL(CUST_POST_NBR, '') "
+                "   , IFNULL(CUST_ADDR, ''), IFNULL(CUST_TEL_NBR, ''), IFNULL(CUST_FAX_NBR, '') "
+                "   , IFNULL(CUST_EMP_NME, ''), IFNULL(CUST_EMP_PHN, ''), IFNULL(CUST_EMAIL, '') "
+                "   , IFNULL(CUST_HOMEP, ''), IFNULL(CUST_GBN, ''), IFNULL(D.RESNAM, ''), IFNULL(CUST_BUNR, '')"
+                "   , IFNULL(CUST_END_CHK, 'Y') "
+                "   FROM MIS1TB003 A "
+                "   LEFT OUTER JOIN OSREFCP D "
+                "   ON A.CUST_GBN = D.RESKEY "
+                "   AND D.RECODE = 'CGB' "
+                "   WHERE CUST_NBR LIKE '%" + custCode + "%' "
+                "   OR CUST_NME LIKE '%" + custCode + "%' "
+                "   AND CUST_GBN LIKE '%" + custType + "%' "
+                "   AND CUST_END_CHK LIKE '%" + custYn + "%' "
+            )
+            custresult = cursor.fetchall()
+            print(custresult)
+
+        # 업체 구분 - 콤보박스
+        with connection.cursor() as cursor:
+            cursor.execute(" SELECT RESKEY, RESNAM FROM OSREFCP WHERE RECODE = 'CGB' ")
+            inputCustType = cursor.fetchall()
+
+        # 사용여부 - 콤보박스
+        with connection.cursor() as cursor:
+            cursor.execute(" SELECT RESKEY, RESNAM FROM OSREFCP WHERE RECODE = 'UST' ")
+            cboCustYn = cursor.fetchall()
+
+        # 업체 분류 - 콤보박스
+        with connection.cursor() as cursor:
+            cursor.execute(" SELECT RESKEY, RESNAM FROM OSREFCP WHERE RECODE = 'CGB' ")
+            cboCustType = cursor.fetchall()
+
+        # 업체군 분류 - 콤보박스
+        with connection.cursor() as cursor:
+            cursor.execute(" SELECT RESKEY, RESNAM FROM OSREFCP WHERE RECODE = 'CGB' ")
+            cboCustType2 = cursor.fetchall()
+
+        return JsonResponse({"inputCustType": inputCustType, "cboCustYn": cboCustYn, "cboCustType": cboCustType, "cboCustType2": cboCustType2
+                                , "custList": custresult})
 
     if custCode is not None and custCode != '':
         with connection.cursor() as cursor:
@@ -139,7 +140,7 @@ def custViews_search(request):
 def custViews_save(request):
     if 'btnSave' in request.POST:
         custName = request.POST.get('txtCustName')
-        custName = request.POST.get('txtCustCode')
+        custCode = request.POST.get('txtCustCode')
         custCeo = request.POST.get('txtCeo')
         custRegNum = request.POST.get('txtRegNum')
         custCat = request.POST.get('txtCustCat')
@@ -157,61 +158,58 @@ def custViews_save(request):
         custType2 = request.POST.get('cboCustType2')
 
         with connection.cursor() as cursor:
-            cursor.execute("INSERT INTO PIS1TB001 "
+            cursor.execute("INSERT INTO MIS1TB003 "
                            "   ("
-                           "     EMP_NBR "
-                           ",    EMP_NME "
-                           ",    EMP_PASS "
-                           ",    EMP_DEPT "
-                           ",    EMP_GBN "
-                           ",    EMP_JO "
-                           ",    EMP_COM "
-                           ",    EMP_TEL "
-                           ",    EMP_IPSA "
-                           ",    EMP_TESA "
-                           ",    EMP_PRC "
-                           ",    EMP_PRC1 "
-                           ",    EMP_PRC2 "
-                           ",    EMP_PRC3 "
-                           ",    CRE_DT "
-                           ",    CRE_USER "
+                           "     CUST_NME "
+                           ",    CUST_NBR "
+                           ",    CUST_CEO_NME "
+                           ",    CUST_ID_NBR "
+                           ",    CUST_BSN_CON "
+                           ",    CUST_BSN_TYP "
+                           ",    CUST_POST_NBR "
+                           ",    CUST_ADDR "
+                           ",    CUST_TEL_NBR "
+                           ",    CUST_FAX_NBR "
+                           ",    CUST_EMP_NME "
+                           ",    CUST_EMP_TEL "
+                           ",    CUST_EMAIL "
+                           ",    CUST_HOMEP "
+                           ",    CUST_GBN "
                            ") "
                            "    VALUES "
                            "    ("
-                           "    '" + empNbr + "'"
-                           ",   '" + str(empNme) + "'"
-                           ",   '" + str(empPass) + "'"
-                           ",   '" + str(empDept) + "'"
-                           ",   '" + str(empGbn) + "'"
-                           ",   '" + str(empJo) + "'"
-                           ",   '" + str(empCom) + "'"
-                           ",   '" + str(empTel) + "'"
-                           ",   '" + str(empIpsa) + "'"
-                           ",   '" + str(empTesa) + "'"
-                           ",   '" + str(empBom) + "'"
-                           ",   '" + str(empBom1) + "'"
-                           ",   '" + str(empBom2) + "'"
-                           ",   '" + str(empBom3) + "'"
-                           ",   '" + str(inepno) + "'"
-                           ",   date_format(now(), '%Y%m%d')"
+                           "    '" + custName + "'"
+                           ",   '" + str(custCode) + "'"
+                           ",   '" + str(custCeo) + "'"
+                           ",   '" + str(custRegNum) + "'"
+                           ",   '" + str(custCat) + "'"
+                           ",   '" + str(custType) + "'"
+                           ",   '" + str(custPostCode) + "'"
+                           ",   '" + str(custAddress) + "'"
+                           ",   '" + str(custTelPhone) + "'"
+                           ",   '" + str(custFax) + "'"
+                           ",   '" + str(custEmp) + "'"
+                           ",   '" + str(custEmpPhone) + "'"
+                           ",   '" + str(custEmail) + "'"
+                           ",   '" + str(custWeb) + "'"
+                           ",   '" + str(custType) + "'"
                            "    ) "
                            "    ON DUPLICATE  KEY "
                            "    UPDATE "
-                           "     EMP_NME  = '" + str(empNme) + "' "
-                           ",    EMP_PASS = '" + str(empPass) + "' "
-                           ",    EMP_DEPT = '" + str(empDept) + "' "
-                           ",    EMP_GBN  = '" + str(empGbn) + "' "
-                           ",    EMP_JO  = '" + str(empJo) + "' "
-                           ",    EMP_COM  = '" + str(empCom) + "' "
-                           ",    EMP_TEL  = '" + str(empTel) + "' "
-                           ",    EMP_IPSA = '" + str(empIpsa) + "' "
-                           ",    EMP_TESA = '" + str(empTesa) + "'  "
-                           ",    EMP_PRC  = '" + str(empBom) + "' "
-                           ",    EMP_PRC1  = '" + str(empBom1) + "' "
-                           ",    EMP_PRC2 = '" + str(empBom2) + "' "
-                           ",    EMP_PRC3 = '" + str(empBom3) + "'  "
-                           ",    UPD_DT = '" + str(utepno) + "' "
-                           ",    UPD_USER = date_format(now(), '%Y%m%d') "
+                           "     CUST_NME  = '" + str(custName) + "' "
+                           ",    CUST_CEO_NME = '" + str(custCeo) + "' "
+                           ",    CUST_ID_NBR = '" + str(custRegNum) + "' "
+                           ",    CUST_BSN_CON  = '" + str(custCat) + "' "
+                           ",    CUST_BSN_TYP  = '" + str(custType) + "' "
+                           ",    CUST_POST_NBR  = '" + str(custPostCode) + "' "
+                           ",    CUST_ADDR  = '" + str(custAddress) + "' "
+                           ",    CUST_TEL_NBR = '" + str(custTelPhone) + "' "
+                           ",    CUST_FAX_NBR = '" + str(custFax) + "'  "
+                           ",    CUST_EMP_NME  = '" + str(custEmp) + "' "
+                           ",    CUST_EMP_TEL  = '" + str(custEmpPhone) + "' "
+                           ",    CUST_EMAIL = '" + str(custEmail) + "' "
+                           ",    CUST_HOMEP = '" + str(custWeb) + "'  "
+                           ",    CUST_GBN = '" + str(custType) + "' "
                            )
             connection.commit()
 
--- apps/views/finance/actBalanceViews.py
+++ apps/views/finance/actBalanceViews.py
@@ -13,3 +13,136 @@ from django.db import connection
 def actBalRegViews(request):
 
     return render(request, "finance/accountBalance-reg.html")
+
+def actBalRegViews_search(request):
+    actCode = request.POST.get('actCode')
+    actNum = request.POST.get('actNum')
+    bankCode = request.POST.get('bankCode')
+
+    if actNum is not None and actNum != '' and bankCode is not None and bankCode != '':
+        with connection.cursor() as cursor:
+            cursor.execute(
+                " SELECT IFNULL(A.ACNUMBER,''), IFNULL(B.ACNUM_NAME,''), IFNULL(B.ACBKCD, ''), IFNULL(C.RESNAM,'') "
+                "        ,IFNULL(A.ACDATE,''), IFNULL(A.ACAMTS, 0), IFNULL(A.ACDESC, '') "
+                "       FROM ACBALANCE A "
+                "       LEFT OUTER JOIN ACNUMBER B "
+                "       ON A.ACNUMBER = B.ACNUMBER "
+                "       LEFT OUTER JOIN OSREFCP C "
+                "       ON B.ACBKCD = C.RESKEY "
+                "       AND C.RECODE = 'BNK' "
+                "       WHERE A.ACNUMBER LIKE '%" + actNum + "%'"
+                "       AND B.ACBKCD LIKE '%" + bankCode + "%'"
+                "       ORDER BY A.ACDATE"
+            )
+            actBalresult = cursor.fetchall()
+
+        # 은행명 - 콤보박스
+        with connection.cursor() as cursor:
+            cursor.execute(" SELECT RESKEY, RESNAM FROM OSREFCP WHERE RECODE = 'BNK' ")
+            cboBankName = cursor.fetchall()
+
+        # 계좌번호 - 콤보박스
+        with connection.cursor() as cursor:
+            cursor.execute(" SELECT ACNUMBER FROM ACNUMBER ")
+            cboActNum = cursor.fetchall()
+
+        return JsonResponse({"cboActNum": cboActNum, "cboBankName": cboBankName, "actBalList": actBalresult})
+
+
+    elif actCode is not None and actCode != '' or bankCode is not None and bankCode != '':
+        # 계좌명 - 콤보박스
+        with connection.cursor() as cursor:
+            cursor.execute(" SELECT ACNUMBER, ACNUM_NAME, ACBKCD, B.RESNAM FROM ACNUMBER A "
+                           "    LEFT OUTER JOIN OSREFCP B "
+                           "    ON A.ACBKCD = B.RESKEY "
+                           "    AND B.RECODE = 'BNK' "
+                           "    WHERE ACNUMBER LIKE '%" + actCode + "%' "
+                           "    AND ACBKCD LIKE '%" + bankCode + "%' ")
+            actResult = cursor.fetchall()
+            print(actResult)
+
+        return JsonResponse({"actList": actResult})
+
+    else:
+        with connection.cursor() as cursor:
+            cursor.execute(
+                " SELECT IFNULL(A.ACNUMBER,''), IFNULL(B.ACNUM_NAME,''), IFNULL(B.ACBKCD, ''), IFNULL(C.RESNAM,'') "
+                "        ,IFNULL(A.ACDATE,''), IFNULL(A.ACAMTS, 0), IFNULL(A.ACDESC, '') "
+                "       FROM ACBALANCE A "
+                "       LEFT OUTER JOIN ACNUMBER B "
+                "       ON A.ACNUMBER = B.ACNUMBER "
+                "       LEFT OUTER JOIN OSREFCP C "
+                "       ON B.ACBKCD = C.RESKEY "
+                "       AND C.RECODE = 'BNK' "
+                "       ORDER BY A.ACDATE"
+            )
+            actBalresult = cursor.fetchall()
+
+            print(actBalresult)
+
+        # 은행명 - 콤보박스
+        with connection.cursor() as cursor:
+            cursor.execute(" SELECT RESKEY, RESNAM FROM OSREFCP WHERE RECODE = 'BNK' ")
+            inputBankType = cursor.fetchall()
+
+        # 은행명 - 콤보박스
+        with connection.cursor() as cursor:
+            cursor.execute(" SELECT RESKEY, RESNAM FROM OSREFCP WHERE RECODE = 'BNK' ")
+            cboBankName = cursor.fetchall()
+
+        return JsonResponse({"inputBankType": inputBankType, "cboBankName": cboBankName, "actBalList": actBalresult})
+
+
+def actBalRegViews_save(request):
+    if 'btnSave' in request.POST:
+        actNum = request.POST.get('cboActNum')
+        actDate = request.POST.get('txtDate')
+        actAmts = request.POST.get('txtBalance')
+        actDesc = request.POST.get('txtRemark')
+
+        with connection.cursor() as cursor:
+            cursor.execute("INSERT INTO ACBALANCE "
+                           "   ("
+                           "     ACNUMBER "
+                           ",    ACDATE "
+                           ",    ACAMTS "
+                           ",    ACDESC "
+                           ") "
+                           "    VALUES "
+                           "    ("
+                           "    '" + actNum + "' "
+                           ",   date_format(now(), '%Y%m%d') "
+                           ",   '" + str(actAmts) + "' "
+                           ",   '" + str(actDesc) + "' "
+                           "    ) "
+                           "    ON DUPLICATE  KEY "
+                           "    UPDATE "
+                           "     ACAMTS  = '" + str(actAmts) + "' "
+                           ",    ACDESC = '" + str(actDesc) + "' "
+                           )
+            connection.commit()
+
+            messages.success(request, '저장 되었습니다.')
+            return render(request, 'finance/accountBalance-reg.html')
+
+    else:
+        messages.warning(request, '입력 하신 정보를 확인 해주세요.')
+        return redirect('/actBalance_reg')
+
+
+
+def actBalRegViews_dlt(request):
+    if request.method == "POST":
+        dataList = json.loads(request.POST.get('arrList'))
+        print(dataList)
+        for act in dataList:
+            acc_split_list = act.split(',')
+            with connection.cursor() as cursor:
+                cursor.execute(" DELETE FROM ACBALANCE WHERE ACNUMBER = '" + acc_split_list[0] + "' "
+                               "                        AND ACDATE = '" + acc_split_list[1] + "' ")
+                connection.commit()
+
+        return JsonResponse({'sucYn': "Y"})
+
+    else:
+        return render(request, 'finance/accountBalance-reg.html')
