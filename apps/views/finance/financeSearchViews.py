07bd3a2c08f31ff08e2c788f735a69bce55cddaf
Choijeongan <choijeongan@gmail.com>
2023-06-22 17:00:33 +0900

    2023.06.22 매입/매출거래내역서 추가

--- apps/views/finance/financeSearchViews.py
+++ apps/views/finance/financeSearchViews.py
@@ -32,12 +32,21 @@ def purTransSearchViews_search(request):
     return JsonResponse({"mainList": mainresult})
 
 def saleTransSearchViews_search(request):
+    Year = request.POST.get('Year')
+
     with connection.cursor() as cursor:
-        cursor.execute(" SELECT IFNULL(ACDATE, ''), IFNULL(DAY(ACDATE), '') "
+        cursor.execute(" SELECT IFNULL(A.ITEM, ''), IFNULL(A.UP_CODE, ''), IFNULL(B.CUST_NME, '')"
+                       "    , IFNULL(A.GUBUN, ''), IFNULL(A.DANGA, ''), IFNULL(SUM(A.QTY), 0)"
+                       "    , IFNULL(SUM(A.SUPPLY), 0), IFNULL(SUM(A.AMTS), 0), IFNULL(DATE_FORMAT(BAL_DD, '%Y%m'), '')"
                        "    , IFNULL(SUM(ACAMTS), 0), IFNULL(ACIOGB, '') "
-                       "    FROM OSBILL  "
-                       "    WHERE ACIOGB = '2' "
-                       "    GROUP BY DAY(ACDATE) ")
+                       "    FROM OSBILL A  "
+                       "    LEFT OUTER JOIN MIS1TB003 B "
+                       "    ON A.UP_CODE = B.CUST_NBR "
+                       "    WHERE GUBUN = '2' "
+                       "    AND YEAR(BAL_DD) = '" + Year + "' "
+                       "    GROUP BY A.ITEM, A.UP_CODE, A.DANGA, A.GUBUN, DATE_FORMAT(BAL_DD, '%Y%m') "
+                       "    ORDER BY A.ITEM, A.UP_CODE, A.DANGA, A.GUBUN, DATE_FORMAT(BAL_DD, '%Y%m') ")
+
         mainresult = cursor.fetchall()
 
     return JsonResponse({"mainList": mainresult})
