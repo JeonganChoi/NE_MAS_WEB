a190c9e5ea81fa4274548667cf6886eabb5d0fd2
thenaeunsys <dsjung0920@gamil.com>
2023-07-23 18:18:15 +0900

    2023.07.23 은행내역서 1차 완료

--- apps/views/currentstate/receivepayViews.py
+++ apps/views/currentstate/receivepayViews.py
@@ -16,13 +16,137 @@ def receivepaySheetViews(request):
 
 
 def receivepaySheetViews_search(request):
-    with connection.cursor() as cursor:
-        cursor.execute(" SELECT RESKEY, RESNAM FROM OSREFCP WHERE RECODE = 'BNK' ")
-        bankresult = cursor.fetchall()
-        bank = bankresult[0][0]
+    cboBank = request.POST.get('cboBank')
+    cboAccount = request.POST.get('cboAccount')
+    strDate = request.POST.get('startDate')
+    endDate = request.POST.get('endDate')
 
-        cursor.execute(" SELECT ACNUMBER FROM ACNUMBER WHERE ACBKCD = '" + bank + "' ")
+    if cboBank != '':
+        with connection.cursor() as cursor:
+            cursor.execute(" SELECT ACNUMBER FROM ACNUMBER WHERE ACBKCD = '" + str(cboBank) + "' ")
 
-        cboAresult = cursor.fetchall()
+            cboAresult = cursor.fetchall()
 
-    return JsonResponse({"cboBank": bankresult, 'cboAccount': cboAresult})
+        with connection.cursor() as cursor:
+            cursor.execute(" SELECT MCODE, MCODENM FROM OSCODEM ")
+
+            titleresult = cursor.fetchall()
+
+        with connection.cursor() as cursor:
+            cursor.execute(" SELECT IFNULL((BALANCE - OAMTS) + IAMTS, 0) AS TOTAL FROM( "
+                           "         SELECT "
+                           "          IFNULL(SUM(A.ACAMTS), 0) AS BALANCE "
+                           "         ,(SELECT IFNULL(SUM(A.ACAMTS), 0) AS OAMTS FROM SISACCTT A LEFT OUTER JOIN ACNUMBER B ON A.ACACNUMBER = B.ACNUMBER WHERE A.ACIOGB = '1' AND A.ACDATE < '" + strDate + "' AND B.ACBKCD = '" + str(cboBank) + "') AS OAMTS "
+                           "         ,(SELECT IFNULL(SUM(A.ACAMTS), 0) AS IAMTS FROM SISACCTT A LEFT OUTER JOIN ACNUMBER B ON A.ACACNUMBER = B.ACNUMBER WHERE A.ACIOGB = '2' AND A.ACDATE < '" + strDate + "' AND B.ACBKCD = '" + str(cboBank) + "') AS IAMTS "
+                           "          FROM ACBALANCE A "
+                           "          LEFT OUTER JOIN ACNUMBER B ON A.ACNUMBER = B.ACNUMBER "
+                           "          WHERE A.ACDATE < '" + strDate + "' AND B.ACBKCD = '" + str(cboBank) + "' "
+                           " ) A ")
+
+            totalresult = cursor.fetchall()
+
+        with connection.cursor() as cursor:
+            cursor.execute(" SELECT IFNULL(UN.ACDATE, ''), IFNULL(UN.ACIOGB, ''), IFNULL(UN.RESNAM, ''), IFNULL(UN.OACAMTS, 0), IFNULL(UN.IACAMTS, 0) "
+                           "      , IFNULL(UN.ACCUST, ''), IFNULL(UN.CUST_NME, ''), IFNULL(UN.ACACNUMBER, ''), IFNULL(UN.ACNUM_NAME, ''), IFNULL(UN.MCODE, ''), IFNULL(UN.MCODENM, '') "
+                           " FROM( "
+                           "         SELECT IFNULL(A.ACDATE, '') AS ACDATE, IFNULL(A.ACIOGB, '') AS ACIOGB, IFNULL(B.RESNAM, '') AS RESNAM, IFNULL(A.ACAMTS, 0) AS OACAMTS, 0 AS IACAMTS "
+                           "              , IFNULL(A.ACCUST, '') AS ACCUST, IFNULL(C.CUST_NME, '') AS CUST_NME "
+                           "             , IFNULL(A.ACACNUMBER, '') AS ACACNUMBER, IFNULL(D.ACNUM_NAME, '') AS ACNUM_NAME, IFNULL(A.MCODE, '') AS MCODE, IFNULL(E.MCODENM, '') AS MCODENM "
+                           "         FROM SISACCTT A "
+                           "         LEFT OUTER JOIN OSREFCP B "
+                           "         ON A.ACIOGB = B.RESKEY "
+                           "         AND B.RECODE = 'OUA' "
+                           "         LEFT OUTER JOIN MIS1TB003 C "
+                           "         ON A.ACCUST = C.CUST_NBR "
+                           "         LEFT OUTER JOIN ACNUMBER D "
+                           "         ON A.ACACNUMBER = D.ACNUMBER "
+                           "         LEFT OUTER JOIN OSCODEM E "
+                           "         ON A.MCODE = E.MCODE "
+                           "         WHERE A.ACIOGB = '1' "
+                           "         UNION ALL "
+                           "         SELECT IFNULL(A.ACDATE, ''), IFNULL(A.ACIOGB, ''), IFNULL(B.RESNAM, ''), 0 AS OACAMTS, IFNULL(A.ACAMTS, 0) AS IACAMTS "
+                           "             , IFNULL(A.ACCUST, ''), IFNULL(C.CUST_NME, ''), IFNULL(A.ACACNUMBER, ''), IFNULL(D.ACNUM_NAME, ''), IFNULL(A.MCODE, ''), IFNULL(E.MCODENM, '') "
+                           "         FROM SISACCTT A "
+                           "         LEFT OUTER JOIN OSREFCP B "
+                           "         ON A.ACIOGB = B.RESKEY "
+                           "         AND B.RECODE = 'OUA' "
+                           "         LEFT OUTER JOIN MIS1TB003 C "
+                           "         ON A.ACCUST = C.CUST_NBR "
+                           "         LEFT OUTER JOIN ACNUMBER D "
+                           "         ON A.ACACNUMBER = D.ACNUMBER "
+                           "         LEFT OUTER JOIN OSCODEM E "
+                           "         ON A.MCODE = E.MCODE "
+                           "         WHERE A.ACIOGB = '2' "
+                           "         ) UN "
+                           " LEFT OUTER JOIN ACNUMBER CC "
+                           " ON UN.ACACNUMBER = CC.ACNUMBER "
+                           " WHERE UN.ACDATE BETWEEN '" + strDate + "' AND '" + endDate + "' AND CC.ACBKCD = '" + str(cboBank) + "'  ORDER BY UN.ACDATE ")
+
+            mainresult = cursor.fetchall()
+
+        return JsonResponse({'cboAccount': cboAresult, 'titleList': titleresult, 'totalList': totalresult, 'mainList': mainresult})
+
+    if cboAccount != '':
+
+        with connection.cursor() as cursor:
+            cursor.execute(" SELECT IFNULL((BALANCE - OAMTS) + IAMTS, 0) AS TOTAL FROM( "
+                           "         SELECT "
+                           "          IFNULL(SUM(ACAMTS), 0) AS BALANCE "
+                           "         ,(SELECT IFNULL(SUM(ACAMTS), 0) AS OAMTS FROM SISACCTT WHERE ACIOGB = '1' AND ACDATE < '" + strDate + "' AND ACACNUMBER = '" + str(cboAccount) + "') AS OAMTS "
+                           "         ,(SELECT IFNULL(SUM(ACAMTS), 0) AS IAMTS FROM SISACCTT WHERE ACIOGB = '2' AND ACDATE < '" + strDate + "' AND ACACNUMBER = '" + str(cboAccount) + "') AS IAMTS "
+                           "          FROM ACBALANCE"
+                           "          WHERE ACDATE < '" + strDate + "' AND ACNUMBER = '" + str(cboAccount) + "' "
+                           " ) A ")
+
+            totalresult = cursor.fetchall()
+
+        with connection.cursor() as cursor:
+            cursor.execute(" SELECT IFNULL(UN.ACDATE, ''), IFNULL(UN.ACIOGB, ''), IFNULL(UN.RESNAM, ''), IFNULL(UN.OACAMTS, 0), IFNULL(UN.IACAMTS, 0) "
+                           "      , IFNULL(UN.ACCUST, ''), IFNULL(UN.CUST_NME, ''), IFNULL(UN.ACACNUMBER, ''), IFNULL(UN.ACNUM_NAME, ''), IFNULL(UN.MCODE, ''), IFNULL(UN.MCODENM, '') "
+                           " FROM( "
+                           "         SELECT IFNULL(A.ACDATE, '') AS ACDATE, IFNULL(A.ACIOGB, '') AS ACIOGB, IFNULL(B.RESNAM, '') AS RESNAM, IFNULL(A.ACAMTS, 0) AS OACAMTS, 0 AS IACAMTS "
+                           "              , IFNULL(A.ACCUST, '') AS ACCUST, IFNULL(C.CUST_NME, '') AS CUST_NME "
+                           "             , IFNULL(A.ACACNUMBER, '') AS ACACNUMBER, IFNULL(D.ACNUM_NAME, '') AS ACNUM_NAME, IFNULL(A.MCODE, '') AS MCODE, IFNULL(E.MCODENM, '') AS MCODENM "
+                           "         FROM SISACCTT A "
+                           "         LEFT OUTER JOIN OSREFCP B "
+                           "         ON A.ACIOGB = B.RESKEY "
+                           "         AND B.RECODE = 'OUA' "
+                           "         LEFT OUTER JOIN MIS1TB003 C "
+                           "         ON A.ACCUST = C.CUST_NBR "
+                           "         LEFT OUTER JOIN ACNUMBER D "
+                           "         ON A.ACACNUMBER = D.ACNUMBER "
+                           "         LEFT OUTER JOIN OSCODEM E "
+                           "         ON A.MCODE = E.MCODE "
+                           "         WHERE A.ACIOGB = '1' "
+                           "         UNION ALL "
+                           "         SELECT IFNULL(A.ACDATE, ''), IFNULL(A.ACIOGB, ''), IFNULL(B.RESNAM, ''), 0 AS OACAMTS, IFNULL(A.ACAMTS, 0) AS IACAMTS "
+                           "             , IFNULL(A.ACCUST, ''), IFNULL(C.CUST_NME, ''), IFNULL(A.ACACNUMBER, ''), IFNULL(D.ACNUM_NAME, ''), IFNULL(A.MCODE, ''), IFNULL(E.MCODENM, '') "
+                           "         FROM SISACCTT A "
+                           "         LEFT OUTER JOIN OSREFCP B "
+                           "         ON A.ACIOGB = B.RESKEY "
+                           "         AND B.RECODE = 'OUA' "
+                           "         LEFT OUTER JOIN MIS1TB003 C "
+                           "         ON A.ACCUST = C.CUST_NBR "
+                           "         LEFT OUTER JOIN ACNUMBER D "
+                           "         ON A.ACACNUMBER = D.ACNUMBER "
+                           "         LEFT OUTER JOIN OSCODEM E "
+                           "         ON A.MCODE = E.MCODE "
+                           "         WHERE A.ACIOGB = '2' "
+                           "         ) UN "
+                           " WHERE UN.ACDATE BETWEEN '" + strDate + "' AND '" + endDate + "' AND UN.ACACNUMBER = '" + str(cboAccount) + "'  ORDER BY UN.ACDATE ")
+
+            mainresult = cursor.fetchall()
+
+        return JsonResponse({'totalList': totalresult, 'mainList': mainresult})
+
+    else:
+        with connection.cursor() as cursor:
+            cursor.execute(" SELECT RESKEY, RESNAM FROM OSREFCP WHERE RECODE = 'BNK' ")
+            bankresult = cursor.fetchall()
+            bank = bankresult[0][0]
+
+            cursor.execute(" SELECT ACNUMBER FROM ACNUMBER WHERE ACBKCD = '" + bank + "' ")
+
+            cboAresult = cursor.fetchall()
+
+        return JsonResponse({"cboBank": bankresult, 'cboAccount': cboAresult})
--- apps/views/currentstate/yearlyMonthlyAccountAnalysisViews.py
+++ apps/views/currentstate/yearlyMonthlyAccountAnalysisViews.py
@@ -29,59 +29,60 @@ def yearlyMontlyAccount_search(request):
     lastYear = int(year) - 1
     actCode = request.POST.get('actCode')
 
-
-
     if year != '' and year is not None and actCode != '' and actCode is not None:
         with connection.cursor() as cursor:
-            cursor.execute(" SELECT  YEAR, IFNULL(SUM(MONTH01), 0) AS MONTH01, IFNULL(SUM(MONTH02), 0) AS MONTH02, IFNULL(SUM(MONTH03), 0) AS MONTH03 "
-                           "              , IFNULL(SUM(MONTH04), 0) AS MONTH04, IFNULL(SUM(MONTH05), 0) AS MONTH05, IFNULL(SUM(MONTH06), 0) AS MONTH06 "
-                           "              , IFNULL(SUM(MONTH07), 0) AS MONTH07, IFNULL(SUM(MONTH08), 0) AS MONTH08, IFNULL(SUM(MONTH09), 0) AS MONTH09 "
-                           "              , IFNULL(SUM(MONTH10), 0) AS MONTH10, IFNULL(SUM(MONTH11), 0) AS MONTH11, IFNULL(SUM(MONTH12), 0) AS MONTH12 "
+            cursor.execute(" SELECT  A.YEAR, IFNULL(SUM(A.MONTH01), 0) AS MONTH01, IFNULL(SUM(A.MONTH02), 0) AS MONTH02, IFNULL(SUM(A.MONTH03), 0) AS MONTH03 "
+                           "               , IFNULL(SUM(A.MONTH04), 0) AS MONTH04, IFNULL(SUM(A.MONTH05), 0) AS MONTH05, IFNULL(SUM(A.MONTH06), 0) AS MONTH06 "
+                           "               , IFNULL(SUM(A.MONTH07), 0) AS MONTH07, IFNULL(SUM(A.MONTH08), 0) AS MONTH08, IFNULL(SUM(MONTH09), 0) AS MONTH09 "
+                           "               , IFNULL(SUM(A.MONTH10), 0) AS MONTH10, IFNULL(SUM(A.MONTH11), 0) AS MONTH11, IFNULL(SUM(MONTH12), 0) AS MONTH12 "
                            " FROM "
                            "     ( SELECT        YEAR(ACDATE) AS YEAR "
-                           "                   , (CASE WHEN MONTH(ACDATE) = '01' THEN SUM(ACAMTS) END) AS MONTH01 "
-                           "                   , (CASE WHEN MONTH(ACDATE) = '02' THEN SUM(ACAMTS) END) AS MONTH02 "
-                           "                   , (CASE WHEN MONTH(ACDATE) = '03' THEN SUM(ACAMTS) END) AS MONTH03 "
-                           "                   , (CASE WHEN MONTH(ACDATE) = '04' THEN SUM(ACAMTS) END) AS MONTH04 "
-                           "                   , (CASE WHEN MONTH(ACDATE) = '05' THEN SUM(ACAMTS) END) AS MONTH05 "
-                           "                   , (CASE WHEN MONTH(ACDATE) = '06' THEN SUM(ACAMTS) END) AS MONTH06 "
-                           "                   , (CASE WHEN MONTH(ACDATE) = '07' THEN SUM(ACAMTS) END) AS MONTH07 "
-                           "                   , (CASE WHEN MONTH(ACDATE) = '08' THEN SUM(ACAMTS) END) AS MONTH08 "
-                           "                   , (CASE WHEN MONTH(ACDATE) = '09' THEN SUM(ACAMTS) END) AS MONTH09 "
-                           "                   , (CASE WHEN MONTH(ACDATE) = '10' THEN SUM(ACAMTS) END) AS MONTH10 "
-                           "                   , (CASE WHEN MONTH(ACDATE) = '11' THEN SUM(ACAMTS) END) AS MONTH11 "
-                           "                   , (CASE WHEN MONTH(ACDATE) = '12' THEN SUM(ACAMTS) END) AS MONTH12 "
+                           "                   , (CASE WHEN YEAR(ACDATE) = '" + str(year) + "' AND MONTH(ACDATE) = '01' AND ACIOGB = '1' THEN SUM(ACAMTS) END) AS MONTH01 "
+                           "                   , (CASE WHEN YEAR(ACDATE) = '" + str(year) + "' AND MONTH(ACDATE) = '02' AND ACIOGB = '1' THEN SUM(ACAMTS) END) AS MONTH02 "
+                           "                   , (CASE WHEN YEAR(ACDATE) = '" + str(year) + "' AND MONTH(ACDATE) = '03' AND ACIOGB = '1' THEN SUM(ACAMTS) END) AS MONTH03 "
+                           "                   , (CASE WHEN YEAR(ACDATE) = '" + str(year) + "' AND MONTH(ACDATE) = '04' AND ACIOGB = '1' THEN SUM(ACAMTS) END) AS MONTH04 "
+                           "                   , (CASE WHEN YEAR(ACDATE) = '" + str(year) + "' AND MONTH(ACDATE) = '05' AND ACIOGB = '1' THEN SUM(ACAMTS) END) AS MONTH05 "
+                           "                   , (CASE WHEN YEAR(ACDATE) = '" + str(year) + "' AND MONTH(ACDATE) = '06' AND ACIOGB = '1' THEN SUM(ACAMTS) END) AS MONTH06 "
+                           "                   , (CASE WHEN YEAR(ACDATE) = '" + str(year) + "' AND MONTH(ACDATE) = '07' AND ACIOGB = '1' THEN SUM(ACAMTS) END) AS MONTH07 "
+                           "                   , (CASE WHEN YEAR(ACDATE) = '" + str(year) + "' AND MONTH(ACDATE) = '08' AND ACIOGB = '1' THEN SUM(ACAMTS) END) AS MONTH08 "
+                           "                   , (CASE WHEN YEAR(ACDATE) = '" + str(year) + "' AND MONTH(ACDATE) = '09' AND ACIOGB = '1' THEN SUM(ACAMTS) END) AS MONTH09 "
+                           "                   , (CASE WHEN YEAR(ACDATE) = '" + str(year) + "' AND MONTH(ACDATE) = '10' AND ACIOGB = '1' THEN SUM(ACAMTS) END) AS MONTH10 "
+                           "                   , (CASE WHEN YEAR(ACDATE) = '" + str(year) + "' AND MONTH(ACDATE) = '11' AND ACIOGB = '1' THEN SUM(ACAMTS) END) AS MONTH11 "
+                           "                   , (CASE WHEN YEAR(ACDATE) = '" + str(year) + "' AND MONTH(ACDATE) = '12' AND ACIOGB = '1' THEN SUM(ACAMTS) END) AS MONTH12 "
                            "              FROM SISACCTT "
                            "              WHERE YEAR(ACDATE) = '" + str(year) + "' "
                            "              AND ACCODE = '" + str(actCode) + "' "
                            "              OR MCODE = '" + str(actCode) + "' "
-                           "              GROUP BY MONTH(ACDATE) ORDER BY MONTH(ACDATE)) A GROUP BY YEAR ")
+                           "              GROUP BY ACDATE, MONTH(ACDATE) "
+                           "              ORDER BY MONTH(ACDATE)) A WHERE A.YEAR IS NOT NULL AND A.YEAR = '" + str(year) + "' GROUP BY A.YEAR ")
             mainresult = cursor.fetchall()
+            print(mainresult)
 
         with connection.cursor() as cursor:
-            cursor.execute(" SELECT  YEAR, IFNULL(SUM(MONTH01), 0) AS MONTH01, IFNULL(SUM(MONTH02), 0) AS MONTH02, IFNULL(SUM(MONTH03), 0) AS MONTH03 "
-                           "              , IFNULL(SUM(MONTH04), 0) AS MONTH04, IFNULL(SUM(MONTH05), 0) AS MONTH05, IFNULL(SUM(MONTH06), 0) AS MONTH06 "
-                           "              , IFNULL(SUM(MONTH07), 0) AS MONTH07, IFNULL(SUM(MONTH08), 0) AS MONTH08, IFNULL(SUM(MONTH09), 0) AS MONTH09 "
-                           "              , IFNULL(SUM(MONTH10), 0) AS MONTH10, IFNULL(SUM(MONTH11), 0) AS MONTH11, IFNULL(SUM(MONTH12), 0) AS MONTH12 "
+            cursor.execute(" SELECT  A.YEAR, IFNULL(SUM(A.MONTH01), 0) AS MONTH01, IFNULL(SUM(A.MONTH02), 0) AS MONTH02, IFNULL(SUM(A.MONTH03), 0) AS MONTH03 "
+                           "               , IFNULL(SUM(A.MONTH04), 0) AS MONTH04, IFNULL(SUM(A.MONTH05), 0) AS MONTH05, IFNULL(SUM(A.MONTH06), 0) AS MONTH06 "
+                           "               , IFNULL(SUM(A.MONTH07), 0) AS MONTH07, IFNULL(SUM(A.MONTH08), 0) AS MONTH08, IFNULL(SUM(A.MONTH09), 0) AS MONTH09 "
+                           "               , IFNULL(SUM(A.MONTH10), 0) AS MONTH10, IFNULL(SUM(A.MONTH11), 0) AS MONTH11, IFNULL(SUM(A.MONTH12), 0) AS MONTH12 "
                            " FROM "
                            "     ( SELECT        YEAR(ACDATE) AS YEAR "
-                           "                   , (CASE WHEN MONTH(ACDATE) = '01' THEN SUM(ACAMTS) END) AS MONTH01 "
-                           "                   , (CASE WHEN MONTH(ACDATE) = '02' THEN SUM(ACAMTS) END) AS MONTH02 "
-                           "                   , (CASE WHEN MONTH(ACDATE) = '03' THEN SUM(ACAMTS) END) AS MONTH03 "
-                           "                   , (CASE WHEN MONTH(ACDATE) = '04' THEN SUM(ACAMTS) END) AS MONTH04 "
-                           "                   , (CASE WHEN MONTH(ACDATE) = '05' THEN SUM(ACAMTS) END) AS MONTH05 "
-                           "                   , (CASE WHEN MONTH(ACDATE) = '06' THEN SUM(ACAMTS) END) AS MONTH06 "
-                           "                   , (CASE WHEN MONTH(ACDATE) = '07' THEN SUM(ACAMTS) END) AS MONTH07 "
-                           "                   , (CASE WHEN MONTH(ACDATE) = '08' THEN SUM(ACAMTS) END) AS MONTH08 "
-                           "                   , (CASE WHEN MONTH(ACDATE) = '09' THEN SUM(ACAMTS) END) AS MONTH09 "
-                           "                   , (CASE WHEN MONTH(ACDATE) = '10' THEN SUM(ACAMTS) END) AS MONTH10 "
-                           "                   , (CASE WHEN MONTH(ACDATE) = '11' THEN SUM(ACAMTS) END) AS MONTH11 "
-                           "                   , (CASE WHEN MONTH(ACDATE) = '12' THEN SUM(ACAMTS) END) AS MONTH12 "
+                           "                   , (CASE WHEN YEAR(ACDATE) = '" + str(lastYear) + "' AND MONTH(ACDATE) = '01' AND ACIOGB = '1' THEN SUM(ACAMTS) END) AS MONTH01 "
+                           "                   , (CASE WHEN YEAR(ACDATE) = '" + str(lastYear) + "' AND MONTH(ACDATE) = '02' AND ACIOGB = '1' THEN SUM(ACAMTS) END) AS MONTH02 "
+                           "                   , (CASE WHEN YEAR(ACDATE) = '" + str(lastYear) + "' AND MONTH(ACDATE) = '03' AND ACIOGB = '1' THEN SUM(ACAMTS) END) AS MONTH03 "
+                           "                   , (CASE WHEN YEAR(ACDATE) = '" + str(lastYear) + "' AND MONTH(ACDATE) = '04' AND ACIOGB = '1' THEN SUM(ACAMTS) END) AS MONTH04 "
+                           "                   , (CASE WHEN YEAR(ACDATE) = '" + str(lastYear) + "' AND MONTH(ACDATE) = '05' AND ACIOGB = '1' THEN SUM(ACAMTS) END) AS MONTH05 "
+                           "                   , (CASE WHEN YEAR(ACDATE) = '" + str(lastYear) + "' AND MONTH(ACDATE) = '06' AND ACIOGB = '1' THEN SUM(ACAMTS) END) AS MONTH06 "
+                           "                   , (CASE WHEN YEAR(ACDATE) = '" + str(lastYear) + "' AND MONTH(ACDATE) = '07' AND ACIOGB = '1' THEN SUM(ACAMTS) END) AS MONTH07 "
+                           "                   , (CASE WHEN YEAR(ACDATE) = '" + str(lastYear) + "' AND MONTH(ACDATE) = '08' AND ACIOGB = '1' THEN SUM(ACAMTS) END) AS MONTH08 "
+                           "                   , (CASE WHEN YEAR(ACDATE) = '" + str(lastYear) + "' AND MONTH(ACDATE) = '09' AND ACIOGB = '1' THEN SUM(ACAMTS) END) AS MONTH09 "
+                           "                   , (CASE WHEN YEAR(ACDATE) = '" + str(lastYear) + "' AND MONTH(ACDATE) = '10' AND ACIOGB = '1' THEN SUM(ACAMTS) END) AS MONTH10 "
+                           "                   , (CASE WHEN YEAR(ACDATE) = '" + str(lastYear) + "' AND MONTH(ACDATE) = '11' AND ACIOGB = '1' THEN SUM(ACAMTS) END) AS MONTH11 "
+                           "                   , (CASE WHEN YEAR(ACDATE) = '" + str(lastYear) + "' AND MONTH(ACDATE) = '12' AND ACIOGB = '1' THEN SUM(ACAMTS) END) AS MONTH12 "
                            "              FROM SISACCTT "
                            "              WHERE YEAR(ACDATE) = '" + str(lastYear) + "' "
                            "              AND ACCODE = '" + str(actCode) + "' "
                            "              OR MCODE = '" + str(actCode) + "' "
-                           "             GROUP BY MONTH(ACDATE) ORDER BY MONTH(ACDATE)) A GROUP BY YEAR ")
+                           "              GROUP BY ACDATE, MONTH(ACDATE) "
+                           "              ORDER BY MONTH(ACDATE)) A WHERE A.YEAR IS NOT NULL AND A.YEAR = '" + str(lastYear) + "' GROUP BY A.YEAR ")
             lastresult = cursor.fetchall()
 
         return JsonResponse({"saleLineList": mainresult, 'lastList': lastresult})
