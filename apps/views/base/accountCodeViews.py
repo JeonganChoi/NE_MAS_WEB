17bebd62e4b116b5ee577dbff444818c0901241c
thenaeunsys <dsjung0920@gamil.com>
2023-08-23 14:58:50 +0900

    2023.08.23 통장 수불장 추가

--- apps/views/base/accountCodeViews.py
+++ apps/views/base/accountCodeViews.py
@@ -21,7 +21,7 @@ def accountCodeViews_search(request):
 
     if mainCode != '' and mainCode is not None:
         with connection.cursor() as cursor:
-            cursor.execute(" SELECT IFNULL(A.MCODE, ''), IFNULL(A.MCODENM, ''), IFNULL(A.MDESC, ''), IFNULL(A.MSEQ, '')"
+            cursor.execute(" SELECT IFNULL(A.MCODE_M, ''), IFNULL(D.RESNAM, ''), IFNULL(A.MCODE, ''), IFNULL(A.MCODENM, ''), IFNULL(A.MDESC, ''), IFNULL(A.MSEQ, '')"
                            "    , IFNULL(A.GBN, ''), IFNULL(B.RESNAM, ''), IFNULL(A.GBN2, ''), IFNULL(C.RESNAM, '') FROM OSCODEM A "
                            "    LEFT OUTER JOIN OSREFCP B "
                            "    ON A.GBN = B.RESKEY "
@@ -29,6 +29,9 @@ def accountCodeViews_search(request):
                            "    LEFT OUTER JOIN OSREFCP C "
                            "    ON A.GBN2 = C.RESKEY "
                            "    AND C.RECODE = 'AGB' "
+                           "    LEFT OUTER JOIN OSREFCP D "
+                           "    ON A.MCODE_M = D.RESKEY "
+                           "    AND D.RECODE = 'MCD' "
                            "    WHERE MCODE = '" + mainCode + "' ")
             mresult = cursor.fetchall()
 
@@ -36,7 +39,7 @@ def accountCodeViews_search(request):
 
     elif mainCode2 != '' and mainCode2 is not None:
         with connection.cursor() as cursor:
-            cursor.execute(" SELECT IFNULL(A.ACODE, ''), IFNULL(A.ACODENM, ''), IFNULL(A.ADESC, ''), IFNULL(A.ASEQ, '')"
+            cursor.execute(" SELECT IFNULL(A.ACODE_M, ''), IFNULL(D.RESNAM, ''), IFNULL(A.ACODE, ''), IFNULL(A.ACODENM, ''), IFNULL(A.ADESC, ''), IFNULL(A.ASEQ, '')"
                            "    , IFNULL(A.GBN, ''), IFNULL(B.RESNAM, ''), IFNULL(A.GBN2, ''), IFNULL(C.RESNAM, '') FROM OSCODEA A "
                            "    LEFT OUTER JOIN OSREFCP B "
                            "    ON A.GBN = B.RESKEY "
@@ -44,6 +47,9 @@ def accountCodeViews_search(request):
                            "    LEFT OUTER JOIN OSREFCP C "
                            "    ON A.GBN2 = C.RESKEY "
                            "    AND C.RECODE = 'AGB' "
+                           "    LEFT OUTER JOIN OSREFCP D "
+                           "    ON A.ACODE_M = D.RESKEY "
+                           "    AND D.RECODE = 'MCD' "
                            "    WHERE ACODE = '" + mainCode2 + "' ")
             aresult = cursor.fetchall()
 
@@ -51,7 +57,7 @@ def accountCodeViews_search(request):
 
     else:
         with connection.cursor() as cursor:
-            cursor.execute(" SELECT IFNULL(A.MCODE, ''), IFNULL(A.MCODENM, ''), IFNULL(A.MSEQ, ''), IFNULL(A.MDESC, '')"
+            cursor.execute(" SELECT IFNULL(A.MCODE_M, ''), IFNULL(D.RESNAM, ''), IFNULL(A.MCODE, ''), IFNULL(A.MCODENM, ''), IFNULL(A.MSEQ, ''), IFNULL(A.MDESC, '')"
                            "    , IFNULL(A.GBN, ''), IFNULL(B.RESNAM, ''), IFNULL(A.GBN2, ''), IFNULL(C.RESNAM, '') FROM OSCODEM A "
                            "    LEFT OUTER JOIN OSREFCP B "
                            "    ON A.GBN = B.RESKEY "
@@ -59,11 +65,14 @@ def accountCodeViews_search(request):
                            "    LEFT OUTER JOIN OSREFCP C "
                            "    ON A.GBN2 = C.RESKEY "
                            "    AND C.RECODE = 'AGB' "
+                           "    LEFT OUTER JOIN OSREFCP D "
+                           "    ON A.MCODE_M = D.RESKEY "
+                           "    AND D.RECODE = 'MCD' "
                            "    WHERE MCODE LIKE '" + str(codeType) + "%' ORDER BY MCODE ")
             mresult = cursor.fetchall()
 
         with connection.cursor() as cursor:
-            cursor.execute(" SELECT IFNULL(A.ACODE, ''), IFNULL(A.ACODENM, ''), IFNULL(A.ASEQ, ''), IFNULL(A.ADESC, '')"
+            cursor.execute(" SELECT IFNULL(A.ACODE_M, ''), IFNULL(D.RESNAM, ''), IFNULL(A.ACODE, ''), IFNULL(A.ACODENM, ''), IFNULL(A.ASEQ, ''), IFNULL(A.ADESC, '')"
                            "    , IFNULL(A.GBN, ''), IFNULL(B.RESNAM, ''), IFNULL(A.GBN2, ''), IFNULL(C.RESNAM, '') FROM OSCODEA A "
                            "    LEFT OUTER JOIN OSREFCP B "
                            "    ON A.GBN = B.RESKEY "
@@ -71,21 +80,32 @@ def accountCodeViews_search(request):
                            "    LEFT OUTER JOIN OSREFCP C "
                            "    ON A.GBN2 = C.RESKEY "
                            "    AND C.RECODE = 'AGB' "
+                           "    LEFT OUTER JOIN OSREFCP D "
+                           "    ON A.ACODE_M = D.RESKEY "
+                           "    AND D.RECODE = 'MCD' "
                            "    WHERE ACODE LIKE '" + str(codeType2) + "%' ORDER BY ACODE ")
             aresult = cursor.fetchall()
 
+        # 상위계정과목
+        with connection.cursor() as cursor:
+            cursor.execute(" SELECT RESKEY, RESNAM FROM OSREFCP WHERE RECODE = 'MCD' ")
+            cboMCode = cursor.fetchall()
+
+        # 구분1
         with connection.cursor() as cursor:
             cursor.execute(" SELECT RESKEY, RESNAM FROM OSREFCP WHERE RECODE = 'CGB' ")
             gbnesult = cursor.fetchall()
 
+        # 구분2
         with connection.cursor() as cursor:
             cursor.execute(" SELECT RESKEY, RESNAM FROM OSREFCP WHERE RECODE = 'AGB' ")
             gbn2result = cursor.fetchall()
 
-        return JsonResponse({"mList": mresult, 'aList': aresult, 'cboGbn': gbnesult, 'cboGbn2': gbn2result})
+        return JsonResponse({"mList": mresult, 'aList': aresult, 'cboMCode': cboMCode, 'cboGbn': gbnesult, 'cboGbn2': gbn2result})
 
 def accountCodeViews_saveM(request):
     codeType = request.POST.get("cboCodeType")
+    mCode_M = request.POST.get("mCode_M")
     mCode = request.POST.get("txtCode_M")
     mCodeNme = request.POST.get("txtCodeNme_M")
     mSeq = request.POST.get("txtSeq_M")
@@ -111,6 +131,7 @@ def accountCodeViews_saveM(request):
               cursor.execute(" INSERT INTO OSCODEM "
                              "   (    "
                              "     MCODE "
+                             ",    MCODE_M "
                              ",    MSEQ "
                              ",    MCODENM "
                              ",    MDESC "
@@ -120,6 +141,7 @@ def accountCodeViews_saveM(request):
                              "    VALUES "
                              "    (   "
                              "    '" + str(mCode) + "'"
+                             ",   '" + str(mCode_M) + "'"
                              ",   (SELECT IFNULL(MAX(A.MSEQ) + 1, 1) AS COUNTED FROM OSCODEM A)"
                              ",   '" + str(mCodeNme) + "'"
                              ",   '" + str(mDesc) + "'"
@@ -139,6 +161,7 @@ def accountCodeViews_saveM(request):
                            ",    GBN = '" + str(gbn) + "' "
                            ",    GBN2 = '" + str(gbn2) + "' "
                            "     WHERE MCODE = '" + str(mCode) + "' "
+                           "     AND MCODE_M = '" + str(mCode_M) + "' "
                            "     AND MSEQ = '" + str(mSeq) + "' "
                            )
             connection.commit()
@@ -150,29 +173,20 @@ def accountCodeViews_saveM(request):
 
 def accountCodeViews_saveA(request):
     codeType2 = request.POST.get("cboCodeType2")
+    aCode_M = request.POST.get("aCode_M")
     aCode = request.POST.get("txtCode_A")
     aCodeNme = request.POST.get("txtCodeNme_A")
+    aDesc = request.POST.get("txtDesc_A")
     aSeq = request.POST.get("txtSeq_A")
     gbn = request.POST.get("cboGbn_A")
     gbn2 = request.POST.get("cboGbn2_A")
 
     if aSeq is None or aSeq == '':
-        # with connection.cursor() as cursor:
-        #     cursor.execute(" SELECT IFNULL(MAX(ACODE), 0) + 1 FROM OSCODEA WHERE ACODE LIKE '" + codeType2 + "%' ")
-        #     subresult = cursor.fetchall()
-        #     x = int(subresult[0][0])
-        #     x = str(x)
-        #     if codeType2 == str(x[0]):
-        #         x = int(x)
-        #         subCode = x + 1
-        #     else:
-        #         subCode = int(codeType2 + str(x).zfill(5)) + 1
-        #         print(subCode)
-
         with connection.cursor() as cursor:
             cursor.execute("INSERT INTO OSCODEA "
                            "   (    "
                            "     ACODE "
+                           ",    ACODE_M "
                            ",    ASEQ "
                            ",    ACODENM "
                            ",    ADESC "
@@ -182,6 +196,7 @@ def accountCodeViews_saveA(request):
                            "    VALUES "
                            "    (   "
                            "    '" + str(aCode) + "'"
+                           ",   '" + str(aCode_M) + "'"
                            ",   (SELECT IFNULL (MAX(A.ASEQ) + 1,1) AS COUNTED FROM OSCODEA A)"
                            ",   '" + str(aCodeNme) + "'"
                            ",   '" + str(aDesc) + "'"
@@ -201,6 +216,7 @@ def accountCodeViews_saveA(request):
                            ",    GBN = '" + str(gbn) + "' "
                            ",    GBN2 = '" + str(gbn2) + "' "
                            "     WHERE ACODE = '" + str(aCode) + "' "
+                           "     AND ACODE_M = '" + str(aCode_M) + "' "
                            "     AND ASEQ = '" + str(aSeq) + "' "
                            )
             connection.commit()
@@ -217,8 +233,9 @@ def accountCodeViews_dltM(request):
         for code in dataList:
             acc_split_list = code.split(',')
             with connection.cursor() as cursor:
-                cursor.execute(" DELETE FROM OSCODEM WHERE MCODE = '" + acc_split_list[0] + "'"
-                               "                       AND MSEQ = '" + acc_split_list[1] + "' ")
+                cursor.execute(" DELETE FROM OSCODEM WHERE MCODE_M = '" + acc_split_list[0] + "'"
+                               "                       AND MCODE = '" + acc_split_list[1] + "' "
+                               "                       AND MSEQ = '" + acc_split_list[2] + "'")
                 connection.commit()
 
         return JsonResponse({'sucYn': "Y"})
@@ -233,8 +250,9 @@ def accountCodeViews_dltA(request):
         for code in dataList:
             acc_split_list = code.split(',')
             with connection.cursor() as cursor:
-                cursor.execute(" DELETE FROM OSCODEA WHERE ACODE = '" + acc_split_list[0] + "'"
-                               "                         AND ASEQ = '" + acc_split_list[1] + "' ")
+                cursor.execute(" DELETE FROM OSCODEA WHERE ACODE_M = '" + acc_split_list[0] + "'"
+                               "                         AND ACODE = '" + acc_split_list[1] + "' "
+                               "                         AND ASEQ = '" + acc_split_list[2] + "'")
                 connection.commit()
 
         return JsonResponse({'sucYn': "Y"})
