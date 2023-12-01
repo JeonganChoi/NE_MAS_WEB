56316c5dc8fc510c51f77b75b934dbdd436d07cf
thenaeunsys <dsjung0920@gamil.com>
2023-12-01 09:17:31 +0900

    2023.11.30 수정

--- apps/views/account/mainViews.py
+++ apps/views/account/mainViews.py
@@ -8,63 +8,69 @@ from django.urls import reverse
 
 
 def loginView(request):
-    userId = request.POST.get('userId')
-    userPw = request.POST.get('userPw')
-    msg = None
+    if request.method == 'GET':
+        return render(request, "account/login.html")
 
-    with connection.cursor() as cursor:
-        cursor.execute("SELECT IFNULL(EMP_NBR, ''), IFNULL(EMP_NME, ''), IFNULL(EMP_DEPT, ''), IFNULL(EMP_GBN, '') "
-                       "        , IFNULL(ICUST, ''), IFNULL(EMP_CHARGE, ''), IFNULL(EMP_TESA, '') "
-                       "    FROM pis1tb001 "
-                       "    WHERE EMP_NBR = '" + str(userId) + "' AND EMP_PASS = '" + str(userPw) + "' ")
-        result = cursor.fetchall()
+    elif request.method == 'POST':
+        userId = request.POST.get('userId')
+        userPw = request.POST.get('userPw')
+        msg = None
 
-        if len(result) == 0:
-            # if len(result) == 0:
-            msg = '아이디 또는 비밀번호가 일치 하지 않습니다.'
-            return render(request, "account/login.html", {'login': "N", "msg": msg})
-        else:
-            tesa = result[0][6]
-            if tesa != '':
-                msg = '사용 불가능한 아이디입니다.'
-                return render(request, "account/login.html", {'login': "N", "msg": msg})
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT IFNULL(EMP_NBR, ''), IFNULL(EMP_NME, ''), IFNULL(EMP_DEPT, ''), IFNULL(EMP_GBN, '') "
+                           "        , IFNULL(ICUST, ''), IFNULL(EMP_CHARGE, ''), IFNULL(EMP_TESA, '') "
+                           "    FROM pis1tb001 "
+                           "    WHERE EMP_NBR = '" + str(userId) + "' AND EMP_PASS = '" + str(userPw) + "' ")
+            result = cursor.fetchall()
+
+            if len(result) == 0:
+                # if len(result) == 0:
+                msg = '아이디 또는 비밀번호가 일치 하지 않습니다.'
+                # return render(request, "account/login.html", {'login': "N", "msg": msg})
+                return JsonResponse({'login': "N", "msg": msg})
             else:
-                USER_NM = result[0][1]
-                USER_GBN = result[0][3]
-                USER_ICUST = result[0][4]
-                if result[0][5] != '':
-                    USER_CHARGE = result[0][5]
+                tesa = result[0][6]
+                if tesa != '':
+                    msg = '사용 불가능한 아이디입니다.'
+                    # return render(request, "account/login.html", {'login': "N", "msg": msg})
+                    return JsonResponse({'login': "N", "msg": msg})
                 else:
-                    USER_CHARGE = 'NO'
-                request.session['userId'] = userId
-                request.session['USER_NM'] = USER_NM
-                request.session['USER_GBN'] = USER_GBN
-                request.session['USER_ICUST'] = USER_ICUST
-                request.session['USER_CHARGE'] = USER_CHARGE
+                    USER_NM = result[0][1]
+                    USER_GBN = result[0][3]
+                    USER_ICUST = result[0][4]
+                    if result[0][5] != '':
+                        USER_CHARGE = result[0][5]
+                    else:
+                        USER_CHARGE = 'NO'
+                    request.session['userId'] = userId
+                    request.session['USER_NM'] = USER_NM
+                    request.session['USER_GBN'] = USER_GBN
+                    request.session['USER_ICUST'] = USER_ICUST
+                    request.session['USER_CHARGE'] = USER_CHARGE
 
-                with connection.cursor() as cursor:
-                    cursor.execute("INSERT INTO LOG_RECODE "
-                                   "(LOG_SEQ"
-                                   ", USER_ID"
-                                   ", USER_NM"
-                                   ", LOGIN_DT"
-                                   ", ICUST"
-                                   ") "
-                                   "VALUES"
-                                   "("
-                                   "(SELECT IFNULL(MAX(LOG_SEQ) + 1, 1) AS LOG_SEQ FROM LOG_RECODE A) "
-                                   ", '" + userId + "'"
-                                   ", '" + USER_NM + "'"
-                                   ", NOW() "
-                                   ", '" + USER_ICUST + "'"
-                                   ")"
-                                   )
-                    connection.commit()
+                    with connection.cursor() as cursor:
+                        cursor.execute("INSERT INTO LOG_RECODE "
+                                       "(LOG_SEQ"
+                                       ", USER_ID"
+                                       ", USER_NM"
+                                       ", LOGIN_DT"
+                                       ", ICUST"
+                                       ") "
+                                       "VALUES"
+                                       "("
+                                       "(SELECT IFNULL(MAX(LOG_SEQ) + 1, 1) AS LOG_SEQ FROM LOG_RECODE A) "
+                                       ", '" + userId + "'"
+                                       ", '" + USER_NM + "'"
+                                       ", NOW() "
+                                       ", '" + USER_ICUST + "'"
+                                       ")"
+                                       )
+                        connection.commit()
 
-            # return render(request, "home/index.html")
-            return JsonResponse({'login': "Y"})
+                # return render(request, "home/index.html")
+                return JsonResponse({'login': "Y"})
 
-        # return render(request, "home/index.html", {})
+            # return render(request, "home/index.html", {})
 
 
 
