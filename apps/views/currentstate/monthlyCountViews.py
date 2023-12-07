2cf26bdcd6cbcba795196eb4857c36a7cd6599b0
thenaeunsys <dsjung0920@gamil.com>
2023-12-07 11:14:06 +0900

    2023.12.07 수정

--- apps/views/currentstate/monthlyCountViews.py
+++ apps/views/currentstate/monthlyCountViews.py
@@ -23,89 +23,35 @@ def montlyCountViews_search(request):
         cursor.execute(" SELECT RESKEY, RESNAM FROM OSREFCP WHERE RECODE = 'MCD' AND ICUST = '" + str(iCust) + "' ")
         headerresult = cursor.fetchall()
 
-    # 매입/매출
+    # 매입
     with connection.cursor() as cursor:
-        cursor.execute(" SELECT IFNULL(A.MCODE, ''), IFNULL(B.MCODENM, ''), IFNULL(B.MCODE_M, '') FROM SISACCTT A "
-                       " LEFT OUTER JOIN OSCODEM B "
-                       " ON A.MCODE = B.MCODE "
-                       " WHERE YEAR(A.ACDATE) = '" + str(year) + "' AND A.ICUST = '" + str(iCust) + "' AND A.MCODE LIKE '51%' "
-                       " GROUP BY A.MCODE, B.MCODENM, B.MCODE_M ")
+        cursor.execute(" SELECT IFNULL(MCODE, ''), IFNULL(MCODENM, ''), IFNULL(MCODE_M, '') FROM OSCODEM  "
+                       " WHERE ICUST = '" + str(iCust) + "' AND MCODE LIKE '51%' "
+                       " GROUP BY MCODE, MCODENM, MCODE_M ")
         inheadresult = cursor.fetchall()
 
     # 매출
     with connection.cursor() as cursor:
-        cursor.execute(" SELECT IFNULL(A.MCODE, ''), IFNULL(B.MCODENM, ''), IFNULL(B.MCODE_M, '') FROM SISACCTT A "
-                       " LEFT OUTER JOIN OSCODEM B "
-                       " ON A.MCODE = B.MCODE "
-                       " WHERE YEAR(A.ACDATE) = '" + str(year) + "' AND A.ICUST = '" + str(iCust) + "' AND A.MCODE LIKE  '41%' "
-                       " GROUP BY A.MCODE, B.MCODENM, B.MCODE_M ")
+        cursor.execute(" SELECT IFNULL(MCODE, ''), IFNULL(MCODENM, ''), IFNULL(MCODE_M, '') FROM OSCODEM  "
+                       " WHERE ICUST = '" + str(iCust) + "' AND MCODE LIKE '41%' "
+                       " GROUP BY MCODE, MCODENM, MCODE_M ")
         outheadresult = cursor.fetchall()
-    # with connection.cursor() as cursor:
-    #     cursor.execute(" SELECT IFNULL(A.OPT, ''), IFNULL(B.MCODENM, ''), IFNULL(B.GBN2, '') FROM OSBILL A LEFT OUTER JOIN OSCODEM B ON A.OPT = B.MCODE "
-    #                    "    WHERE YEAR(BAL_DD) = '" + year + "' AND ICUST = '" + str(iCust) + "'  "
-    #                    "    GROUP BY A.OPT, B.MCODENM, B.GBN2 ")
-    #     headresult = cursor.fetchall()
-    #     print(headresult)
 
     # 입금
     with connection.cursor() as cursor:
-        cursor.execute(" SELECT IFNULL(A.MCODE, ''), IFNULL(B.MCODENM, ''), IFNULL(B.MCODE_M, '') FROM SISACCTT A "
-                       " LEFT OUTER JOIN OSCODEM B "
-                       " ON A.MCODE = B.MCODE "
-                       " WHERE YEAR(A.ACDATE) = '" + str(year) + "' "
-                       " AND A.ICUST = '" + str(iCust) + "' "
-                       " AND A.MCODE LIKE '43%' "
-                       " GROUP BY A.MCODE, B.MCODENM, B.MCODE_M ")
+        cursor.execute(" SELECT IFNULL(MCODE, ''), IFNULL(MCODENM, ''), IFNULL(MCODE_M, '') FROM OSCODEM  "
+                       " WHERE ICUST = '" + str(iCust) + "' AND MCODE LIKE '43%' "
+                       " GROUP BY MCODE, MCODENM, MCODE_M ")
         deheadresult = cursor.fetchall()
 
     # 출금
     with connection.cursor() as cursor:
-        cursor.execute(" SELECT IFNULL(A.MCODE, ''), IFNULL(B.MCODENM, ''), IFNULL(B.MCODE_M, '') FROM SISACCTT A "
-                       " LEFT OUTER JOIN OSCODEM B "
-                       " ON A.MCODE = B.MCODE "
-                       " WHERE YEAR(A.ACDATE) = '" + str(year) + "' "
-                       " AND A.ICUST = '" + str(iCust) + "' "
-                       " AND A.MCODE LIKE '53%' OR A.MCODE LIKE '55%' "
-                       " GROUP BY A.MCODE, B.MCODENM, B.MCODE_M ")
+        cursor.execute(" SELECT IFNULL(MCODE, ''), IFNULL(MCODENM, ''), IFNULL(MCODE_M, '') FROM OSCODEM  "
+                       " WHERE ICUST = '" + str(iCust) + "' AND MCODE LIKE '53%' OR MCODE LIKE '55%' "
+                       " GROUP BY MCODE, MCODENM, MCODE_M ")
         wiheadresult = cursor.fetchall()
-    # with connection.cursor() as cursor:
-    #     cursor.execute(" SELECT A.MCODE, B.MCODENM, B.GBN2 FROM SISACCTT A LEFT OUTER JOIN OSCODEM B ON A.MCODE = B.MCODE"
-    #                    "    WHERE YEAR(ACDATE) = '" + year + "' AND ICUST = '" + str(iCust) + "' "
-    #                    "    GROUP BY A.MCODE, B.MCODENM, B.GBN2 ")
-    #     headresult2 = cursor.fetchall()
-    #     print(headresult2)
 
-    # 매입/매출
-    with connection.cursor() as cursor:
-        cursor.execute("SELECT    IFNULL(SUM(AA.MONTH01), 0) AS MONTH01, IFNULL(SUM(AA.MONTH02), 0) AS MONTH02, IFNULL(SUM(AA.MONTH03), 0) AS MONTH03 "
-                       "        , IFNULL(SUM(AA.MONTH04), 0) AS MONTH04, IFNULL(SUM(AA.MONTH05), 0) AS MONTH05, IFNULL(SUM(AA.MONTH06), 0) AS MONTH06 "
-                       "        , IFNULL(SUM(AA.MONTH07), 0) AS MONTH07, IFNULL(SUM(AA.MONTH08), 0) AS MONTH08, IFNULL(SUM(AA.MONTH09), 0) AS MONTH09 "
-                       "        , IFNULL(SUM(AA.MONTH10), 0) AS MONTH10, IFNULL(SUM(AA.MONTH11), 0) AS MONTH11, IFNULL(SUM(AA.MONTH12), 0) AS MONTH12"
-                       "        , AA.OPT, AA.MCODENM, AA.GBN2, AA.YEAR "
-                       " FROM "
-                       "     ( SELECT        A.OPT, B.MCODENM, B.GBN2, YEAR(BAL_DD) AS YEAR "
-                       "                   , (CASE WHEN MONTH(BAL_DD) = '01' THEN SUM(AMTS) END) AS MONTH01 "
-                       "                   , (CASE WHEN MONTH(BAL_DD) = '02' THEN SUM(AMTS) END) AS MONTH02 "
-                       "                   , (CASE WHEN MONTH(BAL_DD) = '03' THEN SUM(AMTS) END) AS MONTH03 "
-                       "                   , (CASE WHEN MONTH(BAL_DD) = '04' THEN SUM(AMTS) END) AS MONTH04 "
-                       "                   , (CASE WHEN MONTH(BAL_DD) = '05' THEN SUM(AMTS) END) AS MONTH05 "
-                       "                   , (CASE WHEN MONTH(BAL_DD) = '06' THEN SUM(AMTS) END) AS MONTH06 "
-                       "                   , (CASE WHEN MONTH(BAL_DD) = '07' THEN SUM(AMTS) END) AS MONTH07 "
-                       "                   , (CASE WHEN MONTH(BAL_DD) = '08' THEN SUM(AMTS) END) AS MONTH08 "
-                       "                   , (CASE WHEN MONTH(BAL_DD) = '09' THEN SUM(AMTS) END) AS MONTH09 "
-                       "                   , (CASE WHEN MONTH(BAL_DD) = '10' THEN SUM(AMTS) END) AS MONTH10 "
-                       "                   , (CASE WHEN MONTH(BAL_DD) = '11' THEN SUM(AMTS) END) AS MONTH11 "
-                       "                   , (CASE WHEN MONTH(BAL_DD) = '12' THEN SUM(AMTS) END) AS MONTH12 "
-                       "              FROM OSBILL A "
-                       "              LEFT OUTER JOIN OSCODEM B "
-                       "              ON A.OPT = B.MCODE "
-                       "              WHERE YEAR(BAL_DD) = '" + year + "' AND A.ICUST = '" + str(iCust) + "' "
-                       "              GROUP BY A.OPT, B.MCODENM, B.GBN2, MONTH(A.BAL_DD), BAL_DD "
-                       "              ORDER BY MONTH(A.BAL_DD)) AA WHERE AA.YEAR IS NOT NULL AND AA.YEAR = '" + year + "' GROUP BY AA.OPT, AA.MCODENM, AA.GBN2, AA.YEAR ");
-        mainresult = cursor.fetchall()
-        print(mainresult)
-
-    # 입금/출금
+    # 매입
     with connection.cursor() as cursor:
         cursor.execute("  SELECT     IFNULL(SUM(AA.MONTH01), 0) AS MONTH01, IFNULL(SUM(AA.MONTH02), 0) AS MONTH02, IFNULL(SUM(AA.MONTH03), 0) AS MONTH03 "
                        "           , IFNULL(SUM(AA.MONTH04), 0) AS MONTH04, IFNULL(SUM(AA.MONTH05), 0) AS MONTH05, IFNULL(SUM(AA.MONTH06), 0) AS MONTH06 "
@@ -129,12 +75,102 @@ def montlyCountViews_search(request):
                        "              FROM SISACCTT A "
                        "              LEFT OUTER JOIN OSCODEM B "
                        "              ON A.MCODE = B.MCODE "
-                       "              WHERE YEAR(ACDATE) = '" + year + "' "
+                       "              WHERE YEAR(ACDATE) = '" + year + "' AND B.MCODE LIKE '51%' "
+                       "              GROUP BY A.MCODE, B.MCODENM, B.GBN2, MONTH(A.ACDATE), ACDATE "
+                       "              ORDER BY MONTH(A.ACDATE)) AA WHERE AA.YEAR IS NOT NULL AND AA.YEAR = '" + year + "' GROUP BY AA.MCODE, AA.MCODENM, AA.GBN2, AA.YEAR ");
+
+        mainresult = cursor.fetchall()
+
+    # 매출
+    with connection.cursor() as cursor:
+        cursor.execute("  SELECT     IFNULL(SUM(AA.MONTH01), 0) AS MONTH01, IFNULL(SUM(AA.MONTH02), 0) AS MONTH02, IFNULL(SUM(AA.MONTH03), 0) AS MONTH03 "
+                       "           , IFNULL(SUM(AA.MONTH04), 0) AS MONTH04, IFNULL(SUM(AA.MONTH05), 0) AS MONTH05, IFNULL(SUM(AA.MONTH06), 0) AS MONTH06 "
+                       "           , IFNULL(SUM(AA.MONTH07), 0) AS MONTH07, IFNULL(SUM(AA.MONTH08), 0) AS MONTH08, IFNULL(SUM(AA.MONTH09), 0) AS MONTH09 "
+                       "           , IFNULL(SUM(AA.MONTH10), 0) AS MONTH10, IFNULL(SUM(AA.MONTH11), 0) AS MONTH11, IFNULL(SUM(AA.MONTH12), 0) AS MONTH12"
+                       "           , AA.MCODE, AA.MCODENM, AA.GBN2, AA.YEAR "
+                       " FROM "
+                       "     ( SELECT        A.MCODE, B.MCODENM, B.GBN2, YEAR(ACDATE) AS YEAR "
+                       "                   , (CASE WHEN MONTH(ACDATE) = '01' THEN SUM(ACAMTS) END) AS MONTH01 "
+                       "                   , (CASE WHEN MONTH(ACDATE) = '02' THEN SUM(ACAMTS) END) AS MONTH02 "
+                       "                   , (CASE WHEN MONTH(ACDATE) = '03' THEN SUM(ACAMTS) END) AS MONTH03 "
+                       "                   , (CASE WHEN MONTH(ACDATE) = '04' THEN SUM(ACAMTS) END) AS MONTH04 "
+                       "                   , (CASE WHEN MONTH(ACDATE) = '05' THEN SUM(ACAMTS) END) AS MONTH05 "
+                       "                   , (CASE WHEN MONTH(ACDATE) = '06' THEN SUM(ACAMTS) END) AS MONTH06 "
+                       "                   , (CASE WHEN MONTH(ACDATE) = '07' THEN SUM(ACAMTS) END) AS MONTH07 "
+                       "                   , (CASE WHEN MONTH(ACDATE) = '08' THEN SUM(ACAMTS) END) AS MONTH08 "
+                       "                   , (CASE WHEN MONTH(ACDATE) = '09' THEN SUM(ACAMTS) END) AS MONTH09 "
+                       "                   , (CASE WHEN MONTH(ACDATE) = '10' THEN SUM(ACAMTS) END) AS MONTH10 "
+                       "                   , (CASE WHEN MONTH(ACDATE) = '11' THEN SUM(ACAMTS) END) AS MONTH11 "
+                       "                   , (CASE WHEN MONTH(ACDATE) = '12' THEN SUM(ACAMTS) END) AS MONTH12 "
+                       "              FROM SISACCTT A "
+                       "              LEFT OUTER JOIN OSCODEM B "
+                       "              ON A.MCODE = B.MCODE "
+                       "              WHERE YEAR(ACDATE) = '" + year + "' AND B.MCODE LIKE '41%' "
+                       "              GROUP BY A.MCODE, B.MCODENM, B.GBN2, MONTH(A.ACDATE), ACDATE "
+                       "              ORDER BY MONTH(A.ACDATE)) AA WHERE AA.YEAR IS NOT NULL AND AA.YEAR = '" + year + "' GROUP BY AA.MCODE, AA.MCODENM, AA.GBN2, AA.YEAR ");
+
+        mainresult3 = cursor.fetchall()
+
+    # 입금
+    with connection.cursor() as cursor:
+        cursor.execute("  SELECT     IFNULL(SUM(AA.MONTH01), 0) AS MONTH01, IFNULL(SUM(AA.MONTH02), 0) AS MONTH02, IFNULL(SUM(AA.MONTH03), 0) AS MONTH03 "
+                       "           , IFNULL(SUM(AA.MONTH04), 0) AS MONTH04, IFNULL(SUM(AA.MONTH05), 0) AS MONTH05, IFNULL(SUM(AA.MONTH06), 0) AS MONTH06 "
+                       "           , IFNULL(SUM(AA.MONTH07), 0) AS MONTH07, IFNULL(SUM(AA.MONTH08), 0) AS MONTH08, IFNULL(SUM(AA.MONTH09), 0) AS MONTH09 "
+                       "           , IFNULL(SUM(AA.MONTH10), 0) AS MONTH10, IFNULL(SUM(AA.MONTH11), 0) AS MONTH11, IFNULL(SUM(AA.MONTH12), 0) AS MONTH12"
+                       "           , AA.MCODE, AA.MCODENM, AA.GBN2, AA.YEAR "
+                       " FROM "
+                       "     ( SELECT        A.MCODE, B.MCODENM, B.GBN2, YEAR(ACDATE) AS YEAR "
+                       "                   , (CASE WHEN MONTH(ACDATE) = '01' THEN SUM(ACAMTS) END) AS MONTH01 "
+                       "                   , (CASE WHEN MONTH(ACDATE) = '02' THEN SUM(ACAMTS) END) AS MONTH02 "
+                       "                   , (CASE WHEN MONTH(ACDATE) = '03' THEN SUM(ACAMTS) END) AS MONTH03 "
+                       "                   , (CASE WHEN MONTH(ACDATE) = '04' THEN SUM(ACAMTS) END) AS MONTH04 "
+                       "                   , (CASE WHEN MONTH(ACDATE) = '05' THEN SUM(ACAMTS) END) AS MONTH05 "
+                       "                   , (CASE WHEN MONTH(ACDATE) = '06' THEN SUM(ACAMTS) END) AS MONTH06 "
+                       "                   , (CASE WHEN MONTH(ACDATE) = '07' THEN SUM(ACAMTS) END) AS MONTH07 "
+                       "                   , (CASE WHEN MONTH(ACDATE) = '08' THEN SUM(ACAMTS) END) AS MONTH08 "
+                       "                   , (CASE WHEN MONTH(ACDATE) = '09' THEN SUM(ACAMTS) END) AS MONTH09 "
+                       "                   , (CASE WHEN MONTH(ACDATE) = '10' THEN SUM(ACAMTS) END) AS MONTH10 "
+                       "                   , (CASE WHEN MONTH(ACDATE) = '11' THEN SUM(ACAMTS) END) AS MONTH11 "
+                       "                   , (CASE WHEN MONTH(ACDATE) = '12' THEN SUM(ACAMTS) END) AS MONTH12 "
+                       "              FROM SISACCTT A "
+                       "              LEFT OUTER JOIN OSCODEM B "
+                       "              ON A.MCODE = B.MCODE "
+                       "              WHERE YEAR(ACDATE) = '" + year + "' AND B.MCODE LIKE '43%' "
                        "              GROUP BY A.MCODE, B.MCODENM, B.GBN2, MONTH(A.ACDATE), ACDATE "
                        "              ORDER BY MONTH(A.ACDATE)) AA WHERE AA.YEAR IS NOT NULL AND AA.YEAR = '" + year + "' GROUP BY AA.MCODE, AA.MCODENM, AA.GBN2, AA.YEAR ");
 
         mainresult2 = cursor.fetchall()
-        print(mainresult2)
+
+    # 출금
+    with connection.cursor() as cursor:
+        cursor.execute("  SELECT     IFNULL(SUM(AA.MONTH01), 0) AS MONTH01, IFNULL(SUM(AA.MONTH02), 0) AS MONTH02, IFNULL(SUM(AA.MONTH03), 0) AS MONTH03 "
+                       "           , IFNULL(SUM(AA.MONTH04), 0) AS MONTH04, IFNULL(SUM(AA.MONTH05), 0) AS MONTH05, IFNULL(SUM(AA.MONTH06), 0) AS MONTH06 "
+                       "           , IFNULL(SUM(AA.MONTH07), 0) AS MONTH07, IFNULL(SUM(AA.MONTH08), 0) AS MONTH08, IFNULL(SUM(AA.MONTH09), 0) AS MONTH09 "
+                       "           , IFNULL(SUM(AA.MONTH10), 0) AS MONTH10, IFNULL(SUM(AA.MONTH11), 0) AS MONTH11, IFNULL(SUM(AA.MONTH12), 0) AS MONTH12"
+                       "           , AA.MCODE, AA.MCODENM, AA.GBN2, AA.YEAR "
+                       " FROM "
+                       "     ( SELECT        A.MCODE, B.MCODENM, B.GBN2, YEAR(ACDATE) AS YEAR "
+                       "                   , (CASE WHEN MONTH(ACDATE) = '01' THEN SUM(ACAMTS) END) AS MONTH01 "
+                       "                   , (CASE WHEN MONTH(ACDATE) = '02' THEN SUM(ACAMTS) END) AS MONTH02 "
+                       "                   , (CASE WHEN MONTH(ACDATE) = '03' THEN SUM(ACAMTS) END) AS MONTH03 "
+                       "                   , (CASE WHEN MONTH(ACDATE) = '04' THEN SUM(ACAMTS) END) AS MONTH04 "
+                       "                   , (CASE WHEN MONTH(ACDATE) = '05' THEN SUM(ACAMTS) END) AS MONTH05 "
+                       "                   , (CASE WHEN MONTH(ACDATE) = '06' THEN SUM(ACAMTS) END) AS MONTH06 "
+                       "                   , (CASE WHEN MONTH(ACDATE) = '07' THEN SUM(ACAMTS) END) AS MONTH07 "
+                       "                   , (CASE WHEN MONTH(ACDATE) = '08' THEN SUM(ACAMTS) END) AS MONTH08 "
+                       "                   , (CASE WHEN MONTH(ACDATE) = '09' THEN SUM(ACAMTS) END) AS MONTH09 "
+                       "                   , (CASE WHEN MONTH(ACDATE) = '10' THEN SUM(ACAMTS) END) AS MONTH10 "
+                       "                   , (CASE WHEN MONTH(ACDATE) = '11' THEN SUM(ACAMTS) END) AS MONTH11 "
+                       "                   , (CASE WHEN MONTH(ACDATE) = '12' THEN SUM(ACAMTS) END) AS MONTH12 "
+                       "              FROM SISACCTT A "
+                       "              LEFT OUTER JOIN OSCODEM B "
+                       "              ON A.MCODE = B.MCODE "
+                       "              WHERE YEAR(ACDATE) = '" + year + "' AND B.MCODE LIKE '53%' OR B.MCODE LIKE '55%' "
+                       "              GROUP BY A.MCODE, B.MCODENM, B.GBN2, MONTH(A.ACDATE), ACDATE "
+                       "              ORDER BY MONTH(A.ACDATE)) AA WHERE AA.YEAR IS NOT NULL AND AA.YEAR = '" + year + "' GROUP BY AA.MCODE, AA.MCODENM, AA.GBN2, AA.YEAR ");
+
+        mainresult4 = cursor.fetchall()
 
     return JsonResponse({"headerList": headerresult, "inheadList": inheadresult, "outheadList": outheadresult
-                            , 'deheadList': deheadresult, 'wiheadList': wiheadresult, 'mainList': mainresult, 'mainList2': mainresult2})
+                            , 'deheadList': deheadresult, 'wiheadList': wiheadresult
+                            , 'mainList': mainresult, 'mainList2': mainresult2, "mainList3": mainresult3, "mainList4": mainresult4})
